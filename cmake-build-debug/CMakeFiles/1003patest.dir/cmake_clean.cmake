3021303705733b8ad9dc5b7a0d8bff8a812e8cbb
tuanhsiang@163.com <tuanhsiang@163.com>
2020-12-18 00:33:08 +0800

    log3

---
+++ cmake-build-debug/CMakeFiles/1003patest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1003patest.exe"
+  "1003patest.exe.manifest"
+  "1003patest.pdb"
+  "CMakeFiles/1003patest.dir/1003patest.cpp.obj"
+  "lib1003patest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1003patest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
